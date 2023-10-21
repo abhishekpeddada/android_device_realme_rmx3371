ead97065406d601437b0478adec7244e74867a56
abhishekpeddada <abhishekpeddada@gmail.com>
2023-10-21 20:17:36 +0530

    drop oplus cam

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
 git clone https://github.com/abhishekpeddada/kernel_realme_sm8250 kernel/realme/sm8250 --depth=1
 git clone https://github.com/abhishekpeddada/vendor_realme_spartan vendor/realme/spartan --depth=1
 git clone https://github.com/abhishekpeddada/android_hardware_oplus hardware/oplus
-git clone https://gitlab.com/pjgowtham/proprietary_vendor_oplus_camera/ vendor/oplus/camera
