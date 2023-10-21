f6a17cecc2d4bdac2a22a9f537b089104a726306
abhishekpeddada <abhishekpeddada@gmail.com>
2023-10-21 20:16:48 +0530

    Update vendorsetup

--- superior_spartan.mk
+++ superior_spartan.mk
@@ -8,8 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-#Inherit oplus-camera
-$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
 
 # Inherit from spartan device
 $(call inherit-product, device/realme/spartan/device.mk)
