afe753a838334baa8832f5cbc402d8e6a9b675a3
33bca <fabian.leutenegger@bluewin.ch>
2023-12-05 17:29:22 +0530

    spartan: Link libdlbdsservice and libhwdap against v33 libstagefright… …_foundation
     * fixes crashes and absurdly high CPU usage while using dolby
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Signed-off-by: DragonEmperor9480 <amruteshnaregal1234@gmail.com>
    Change-Id: Ib72b775dae677f3085d0b0e2afcbb709d8690063

--- extract-files.sh
+++ extract-files.sh
@@ -93,6 +93,9 @@ function blob_fixup() {
         odm/lib64/liboplus_service.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
+        odm/lib/libdlbdsservice_v3_6.so | odm/lib/libstagefright_soft_ddpdec.so | odm/lib/libstagefrightdolby.so | odm/lib64/libdlbdsservice_v3_6.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
